e83461baec9b3b6cea1a8b1781971b0730d9913b
J. B. Rainsberger <me@jbrains.ca>
2017-11-20 14:00:34 +0100

    We can add non-integer fractions as long as their denominators are the same.

--- Fractions/src/test/java/ca/jbrains/math/test/AddFractionsTest.java
+++ Fractions/src/test/java/ca/jbrains/math/test/AddFractionsTest.java
@@ -31,19 +31,46 @@ public class AddFractionsTest {
         Assert.assertEquals(12, sum.intValue());
     }
 
+    @Test
+    public void notIntegersWithSameDenominator() throws Exception {
+        final Fraction sum = new Fraction(1, 5).plus(new Fraction(2, 5));
+        Assert.assertEquals(3, sum.getNumerator());
+        Assert.assertEquals(5, sum.getDenominator());
+    }
+
     private static class Fraction {
-        private final int intValue;
+        private  int numerator;
+        private  int denominator;
+        private int intValue;
 
         public Fraction(final int intValue) {
             this.intValue = intValue;
         }
 
+        public Fraction(final int numerator, final int denominator) {
+            this.numerator = numerator;
+            this.denominator = denominator;
+        }
+
         public Fraction plus(final Fraction that) {
-            return new Fraction(this.intValue + that.intValue);
+            if (this.denominator == 0 && that.denominator == 0) {
+                return new Fraction(this.intValue + that.intValue);
+            }
+            else {
+                return new Fraction(this.numerator + that.numerator, this.denominator);
+            }
         }
 
         public int intValue() {
             return intValue;
+        }
+
+        public int getNumerator() {
+            return numerator;
+        }
+
+        public int getDenominator() {
+            return denominator;
         }
     }
 }
