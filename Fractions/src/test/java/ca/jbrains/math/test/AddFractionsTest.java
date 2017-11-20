3a56c2dcf7fb8cb843f62dee4b6def30d89517f0
J. B. Rainsberger <me@jbrains.ca>
2017-11-20 14:23:23 +0100

    Removed obsolete code.

--- Fractions/src/test/java/ca/jbrains/math/test/AddFractionsTest.java
+++ Fractions/src/test/java/ca/jbrains/math/test/AddFractionsTest.java
@@ -69,15 +69,6 @@ public class AddFractionsTest {
                     this.denominator * that.denominator);
         }
 
-        public int getNumerator() {
-            return numerator;
-        }
-
-        public int getDenominator() {
-            return denominator;
-        }
-
-        @Override
         public boolean equals(final Object other) {
             if (other instanceof Fraction) {
                 Fraction that = (Fraction) other;
