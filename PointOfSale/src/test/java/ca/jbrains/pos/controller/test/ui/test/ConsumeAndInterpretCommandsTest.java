eaf641fd91fe421328ceba9e093766f0c3a0e772
J. B. Rainsberger <me@jbrains.ca>
2017-11-22 11:39:58 +0100

    We can consume text input as commands as long as there are 0-1 lines of input.

--- PointOfSale/src/test/java/ca/jbrains/pos/controller/test/ui/test/ConsumeAndInterpretCommandsTest.java
+++ PointOfSale/src/test/java/ca/jbrains/pos/controller/test/ui/test/ConsumeAndInterpretCommandsTest.java
@@ -5,6 +5,8 @@ import org.jmock.integration.junit4.JUnitRuleMockery;
 import org.junit.Rule;
 import org.junit.Test;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.Reader;
 import java.io.StringReader;
 import java.util.Collections;
@@ -24,11 +26,24 @@ public class ConsumeAndInterpretCommandsTest {
         consumeAndInterpretCommands(new StringReader(unlines(Collections.emptyList())));
     }
 
+    @Test
+    public void oneCommand() throws Exception {
+        context.checking(new Expectations() {{
+            oneOf(barcodeScannedListener).onBarcode("127638");
+        }});
+
+        consumeAndInterpretCommands(new StringReader(unlines(Collections.singletonList("127638"))));
+    }
+
     private static String unlines(final List<CharSequence> linesOfText) {
         return String.join(System.lineSeparator(), linesOfText);
     }
 
-    private void consumeAndInterpretCommands(final Reader reader) {
+    private void consumeAndInterpretCommands(final Reader reader) throws IOException {
+        final String theLine = new BufferedReader(reader).readLine();
+        if (theLine != null) {
+            barcodeScannedListener.onBarcode(theLine);
+        }
     }
 
     public interface BarcodeScannedListener {
